a9e31b429f461e4987a0a3f939ae9e50245bf39c
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-11-08 00:39:51 +1100

    Add Auth

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link , Redirect } from 'react-router-dom';
 import "./card.css";
 
 import { Row, Col , Card, Button, CardGroup } from "react-bootstrap";
+
+import Auth from '../../utils/auth';
 
 const styles = {
   card :{
@@ -32,7 +34,9 @@ const styles = {
     backgroundColor: "goldenrod",
     borderColor: "black",
     transition: "all ease .5s",  
-
+    width: "265px",
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }
 
@@ -47,7 +51,10 @@ return (
   <CardGroup>
     <Col>
       <Card className="m-6 border-0 shadow" style={styles.card}>
-      <Card.Header>Featured</Card.Header>
+      {props.meeting.onLine==true ? (
+      <Card.Header>Featured (Online)</Card.Header> ) :(
+        <Card.Header>Featured</Card.Header>
+      ) }
         <Card.Img {...styles.cardImage} variant="top" src={props.meeting.meetingPhoto} alt={props.meeting.title} />
         <Card.Body>
           <Card.Title style={styles.cardTitle}> {props.meeting.title} </Card.Title>
@@ -55,16 +62,25 @@ return (
           {description}
           </Card.Text>          
         </Card.Body>
-        <Link
-            // className="btn btn-primary btn-block btn-squared"
-            to={`/meetings/${props.meeting._id}`}
-          >
-        <Button style={styles.btn} variant="primary" className="btn1">Click for details</Button>
-        </Link>
-        <Card.Footer>
-          <small className="text-muted">Event Date & Time : <strong>{props.meeting.date}</strong></small>
-        </Card.Footer>
-      </Card>
+        {Auth.loggedIn() ? (
+            <>
+            <Link
+                // className="btn btn-primary btn-block btn-squared"
+                to={`/meeting/${props.meeting._id}`}
+              > 
+              <Button style={styles.btn} variant="primary" className="btn1">Click for details</Button>
+              </Link>
+            </>
+          ) : (    
+            <Link to="/login">
+            <Button style={styles.btn} variant="primary" className="btn1">Login for details</Button>     
+            </Link>             
+           )}         
+          
+          <Card.Footer>
+            <small className="text-muted">Event Date & Time : <strong>{props.meeting.date}</strong></small>
+          </Card.Footer>
+        </Card>
     </Col>
   </CardGroup>
 
